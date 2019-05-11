221960c1358bdc7d5b1a9abb416b4d246b5f6f02
xlq246860 <2797240852@qq.com>
2019-05-11 16:01:55 +0800

    Site updated: 2019-05-11 16:01:50

---
+++ about/js/bot.js
@@ -0,0 +1,80 @@
+
+var text = $("#f-left");
+text.focus();
+function action() 
+{
+	if(text.val()==null||text.val()=="")
+	{
+		text.focus();
+		return;
+	}
+
+	$(".b-body").append("<div class="mWord"><span></span><p>" + text.val() + "</p></div>");
+	$(".b-body").scrollTop(10000000);
+	
+	var args= {
+			type : "get",
+			url:"https://api.ownthink.com/bot",
+			data : {"appid" : "7ad626567c7efc6b300c05eb228ea37c", "spoken" : text.val()},
+			success : function(redata)
+			{
+				var my_data = $.parseJSON(redata)
+
+				var array= [my_data.data.info.text];
+
+				if(my_data.data.info.hasOwnProperty("heuristic"))
+				{
+					for (var i=0; i < my_data.data.info.heuristic.length; i++)
+					{
+						array.push(my_data.data.info.heuristic[i]);
+					}
+				} 
+
+				for (var i=0; i < array.length; i++)
+				{
+					// ��ʽ   console.log(array[i]);
+					var result = array[i];
+					$(".b-body").append("<div class="rotWord"><span></span> <p id="member">" + result + "</p></div>");
+					$(".b-body").scrollTop(10000000);
+				}
+			}
+		}
+	
+	ajax(args);
+	text.val("");
+	text.focus();
+	
+};
+
+$("#btn").click(function()
+{
+	action();
+});
+$(document).keydown(function(event)
+{
+	if(event.keyCode==13)
+	{
+		action();
+	}
+});
+
+function ajax(mJson)
+{
+	var type=mJson.type||'get';
+	var url=mJson.url;
+	var data=mJson.data;
+	var success=mJson.success;
+	var error=mJson.error;
+	var dataStr='';
+	
+	if(data)
+	{
+		var arr = Object.keys(data);
+		var len = arr.length;
+		var i = 0;
+		
+		for (var key in data)
+		{
+			dataStr+=key+'='+data[key];
+	
+			if (++i<len) {="" datastr+="&" ;="" }="" if(type.tolowercase()="='get')" url+="?" +datastr;="" console.log(url);="" var="" xhr="new" xmlhttprequest();="" xhr.open(type,url,true);="" xhr.setrequestheader('content-type'="" ,="" 'application="" x-www-form-urlencoded');="" xhr.send(null);="" xhr.onreadystatechange="function()" if(xhr.readystate="=4)" if(xhr.status="">=200&&xhr.status</len)>
